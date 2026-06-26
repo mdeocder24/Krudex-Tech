72783566f1a5347c88f21a7e98933ff44eec3b6a
mdecoder24 <krkushwant@gmail.com>
2026-06-27 02:08:00 +0530

    feat: implement landing page layout with integrated components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
+import Partners from '@/components/Partners';
 import Stats from '@/components/Stats';
 import Services from '@/components/Services';
 import CTA from '@/components/CTA';
@@ -10,6 +11,7 @@ export default function Home() {
     <main className="min-h-screen bg-transparent selection:bg-krudex-blue selection:text-krudex-black flex flex-col">
       <Navbar />
       <Hero />
+      <Partners />
       <Stats />
       <Services />
       <CTA />
